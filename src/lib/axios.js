edfd2b2932a430fa598afd6edc8c460bc2e21126
Oluwatimie97 <timilehinolarewaju9@gmail.com>
2026-06-30 07:17:46 -0700

    adding backend url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "https://same-day-loan-backend.vercel.app/api";
-
-// Create axios instance with default config
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
+
 export default api;
